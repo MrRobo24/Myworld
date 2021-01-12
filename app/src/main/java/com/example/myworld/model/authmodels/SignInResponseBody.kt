24d160263090c9d22ab0f8f168d1d33b5254d650
MrRobo24 <sumanarpit.24@gmail.com>
2021-01-12 16:54:03 +0530

    Sign In response initialization fixed

--- app/src/main/java/com/example/myworld/model/authmodels/SignInResponseBody.kt
+++ app/src/main/java/com/example/myworld/model/authmodels/SignInResponseBody.kt
@@ -1,3 +1,3 @@
 package com.example.myworld.model.authmodels
 
-data class SignInResponseBody(var token: String, var userId: Int, var email: String)
+data class SignInResponseBody(var token: String, var user_id: Int, var email: String)
